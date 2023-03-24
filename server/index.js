ea1d2834133ff40fe9cf26ff41cd60d3302fdae0
Gunjan Tunga <gunjan.kumar@lecturenotes.in>
2023-03-24 17:50:17 +0530

    ignore nodemodules

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import Routes from './routes/index.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 app.use('/', Routes);
 
-const url = "mongodb+srv://gunjankumartunga12:gunjan123@cluster0.blmpx9a.mongodb.net/quiz?retryWrites=true&w=majority";
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const url = `mongodb+srv://${username}:${password}@cluster0.blmpx9a.mongodb.net/quiz?retryWrites=true&w=majority`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).
     then(() => {
         console.log(`connected successfully`);
